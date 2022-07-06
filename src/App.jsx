e150aa4174b71c0bd2832e9836d81ec449db9d1a
Catherine Tung <catt.tung@gmail.com>
2022-07-06 15:56:12 -0400

    Clean up app jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <img className='App-logo' src={logo} alt="" />
+    </>
+  );
+}
+
+export default App;
