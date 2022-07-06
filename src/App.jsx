6e93d6d5d5749d29c8acd966bfbe780444bf5996
Catherine Tung <catt.tung@gmail.com>
2022-07-06 16:03:20 -0400

    Add routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-    <img className='App-logo' src={logo} alt="" />
+      <img className='App-logo' src={logo} alt="" />
+      <h1>Harry Potter Characters</h1>
+      <Routes>
+      </Routes>
     </>
   );
 }
